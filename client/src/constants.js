45668cea36c09313b9f4e6b719e8735768eee781
Ella Rashty <48799296+EllaRashty@users.noreply.github.com>
2023-07-20 19:59:32 +0300

    Delete client directory

--- client/src/constants.js
+++
@@ -1,9 +0,0 @@
-export const constants = {
-  OCCUPIED: `The Room Is Occupied ⮕ `,
-  GO_BACK: ` ⬅ Lobby`,
-  RETURN: `Return To Lobby`,
-  NOT_FOUND: `Page Not Found `,
-  STUDENT: `Student Position - Edit Permission`,
-  MENTOR: `Mentor Position - Read Only Permission`,
-  MENTOR_TEXT: `/* Waiting for the student to start typing /*`,
-};
