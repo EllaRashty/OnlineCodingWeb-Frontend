a9060f7b7136b15397f93c8a9b78ef2ff25d2f6d
Ella Rashty <48799296+EllaRashty@users.noreply.github.com>
2023-07-21 11:45:25 +0300

    Update firebaseConfig.js

--- client/src/components/firebaseConfig/firebaseConfig.js
+++ client/src/components/firebaseConfig/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHr7sWyOo-OsbqpcWsiuIVaV7I8w1vqW8",
-  authDomain: "codeblock-74967.firebaseapp.com",
-  projectId: "codeblock-74967",
-  storageBucket: "codeblock-74967.appspot.com",
-  messagingSenderId: "377958881263",
-  appId: "1:377958881263:web:d04d70a21f33fc0e39081a"
+  apiKey: "AIzaSyDlhCJmQcm0cpRLVq3PlL062Mv6-JYnnxo",
+  authDomain: "codeblock-376c1.firebaseapp.com",
+  projectId: "codeblock-376c1",
+  storageBucket: "codeblock-376c1.appspot.com",
+  messagingSenderId: "163240305260",
+  appId: "1:163240305260:web:b602088f28d8b7eeed2e49"
 };
 
 // Initialize Firebase
